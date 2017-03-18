fed675f9f80700df13d761459db51005a1522159
Simone Rubino <daemo00@gmail.com>
2017-03-18 21:09:55 +0100

    fixed chatHead

--- app/src/main/java/com/daemo/myfirstapp/chatHeads/ChatHeadService.java
+++ app/src/main/java/com/daemo/myfirstapp/chatHeads/ChatHeadService.java
@@ -1,9 +1,12 @@
 package com.daemo.myfirstapp.chatHeads;
 
+import android.app.Activity;
 import android.app.Service;
 import android.content.Intent;
 import android.graphics.PixelFormat;
 import android.os.IBinder;
+import android.support.v4.os.ResultReceiver;
+import android.util.Log;
 import android.view.Gravity;
 import android.view.MotionEvent;
 import android.view.View;
@@ -11,18 +14,27 @@ import android.view.WindowManager;
 import android.widget.ImageView;
 
 import com.daemo.myfirstapp.R;
+import com.daemo.myfirstapp.common.Constants;
+import com.daemo.myfirstapp.common.Utils;
 
 public class ChatHeadService extends Service {
 
     private WindowManager windowManager;
     private ImageView chatHead;
     WindowManager.LayoutParams params;
+    private ResultReceiver resultReceiver;
 
     @Override
-    public void onCreate() {
-        super.onCreate();
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        Log.d(Utils.getTag(this), "onStartCommand(" + Utils.debugIntent(intent) + ", " + flags + ", " + startId + ")");
+        if (intent != null) resultReceiver = intent.getParcelableExtra(Constants.Location.RECEIVER);
 
         windowManager = (WindowManager) getSystemService(WINDOW_SERVICE);
+        if (chatHead != null) {
+            windowManager.removeView(chatHead);
+            attachChatHead();
+            return super.onStartCommand(intent, flags, startId);
+        }
 
         chatHead = new ImageView(this);
         chatHead.setImageResource(R.drawable.empty_photo);
@@ -38,7 +50,7 @@ public class ChatHeadService extends Service {
         params.x = 0;
         params.y = 100;
 
-        //this code is for dragging the chat head
+        // This code is for dragging the chat head
         chatHead.setOnTouchListener(new View.OnTouchListener() {
             private int initialX;
             private int initialY;
@@ -67,19 +79,30 @@ public class ChatHeadService extends Service {
                 return false;
             }
         });
+        attachChatHead();
+
+        return super.onStartCommand(intent, flags, startId);
+    }
+
+    public void attachChatHead() {
         windowManager.addView(chatHead, params);
+        Constants.SERVICE_RUNNING_CHATHEAD = true;
+        if (resultReceiver != null) resultReceiver.send(Activity.RESULT_OK, null);
     }
 
     @Override
     public void onDestroy() {
         super.onDestroy();
+        Log.d(Utils.getTag(this), "onDestroy");
         if (chatHead != null)
             windowManager.removeView(chatHead);
+        Constants.SERVICE_RUNNING_CHATHEAD = false;
+        if (resultReceiver != null) resultReceiver.send(Activity.RESULT_CANCELED, null);
     }
 
     @Override
     public IBinder onBind(Intent intent) {
-        // TODO Auto-generated method stub
+        Log.d(Utils.getTag(this), "onBind(" + Utils.debugIntent(intent) + ")");
         return null;
     }
 }
--- app/src/main/java/com/daemo/myfirstapp/chatHeads/ChatHeadsActivity.java
+++ app/src/main/java/com/daemo/myfirstapp/chatHeads/ChatHeadsActivity.java
@@ -4,62 +4,94 @@ import android.content.Intent;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
+import android.os.Handler;
 import android.provider.Settings;
+import android.support.v4.os.ResultReceiver;
 import android.view.View;
 import android.widget.Button;
 
 import com.daemo.myfirstapp.MySuperActivity;
 import com.daemo.myfirstapp.R;
+import com.daemo.myfirstapp.common.Constants;
+import com.daemo.myfirstapp.common.Utils;
 
 public class ChatHeadsActivity extends MySuperActivity {
+
+    private Button startService;
+    private Button stopService;
+    private ResultReceiver resultReceiver = new ResultReceiver(new Handler()) {
+        @Override
+        protected void onReceiveResult(int resultCode, Bundle resultData) {
+            super.onReceiveResult(resultCode, resultData);
+            refreshUI();
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chat_heads);
-        checkDrawOverlayPermission();
-        Button startService = (Button) findViewById(R.id.startService);
-        Button stopService = (Button) findViewById(R.id.stopService);
+        startService = (Button) findViewById(R.id.startService);
+        stopService = (Button) findViewById(R.id.stopService);
+
         startService.setOnClickListener(new View.OnClickListener() {
 
             @Override
             public void onClick(View v) {
-                startService(new Intent(getApplication(), ChatHeadService.class));
-
+                if (checkDrawOverlayPermission()) {
+                    Intent intent = new Intent(getApplication(), ChatHeadService.class);
+                    intent.putExtra(Constants.Location.RECEIVER, resultReceiver);
+                    startService(intent);
+                }
             }
         });
         stopService.setOnClickListener(new View.OnClickListener() {
 
             @Override
             public void onClick(View v) {
-                stopService(new Intent(getApplication(), ChatHeadService.class));
-
+                if (checkDrawOverlayPermission()) {
+                    Intent intent = new Intent(getApplication(), ChatHeadService.class);
+                    intent.putExtra(Constants.Location.RECEIVER, resultReceiver);
+                    stopService(intent);
+                }
             }
         });
     }
 
     @Override
-    protected int getLayoutResID() {
-        return R.layout.activity_chat_heads;
+    protected void onStart() {
+        super.onStart();
+        refreshUI();
+        if (Utils.isMyServiceRunning(this, ChatHeadService.class))
+            startService.performClick();
     }
 
-    public final static int REQUEST_CODE = 1;
+    private void refreshUI() {
+        boolean service_started = Utils.isMyServiceRunning(this, ChatHeadService.class);
+        startService.setEnabled(!service_started);
+        stopService.setEnabled(service_started);
+    }
 
-    public void checkDrawOverlayPermission() {
+    public boolean checkDrawOverlayPermission() {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
             if (!Settings.canDrawOverlays(this)) {
                 Intent intent = new Intent(Settings.ACTION_MANAGE_OVERLAY_PERMISSION, Uri.parse("package:" + getPackageName()));
-                startActivityForResult(intent, REQUEST_CODE);
-            }
+                startActivityForResult(intent, Constants.REQUEST_CODE_OVERLAY_PERMISSION);
+            } else
+                return true;
         }
+        return false;
     }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        if (requestCode == REQUEST_CODE) {
+        if (requestCode == Constants.REQUEST_CODE_OVERLAY_PERMISSION) {
             if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
                 if (Settings.canDrawOverlays(this)) {
-                    // continue here - permission was granted
+                    if (!Utils.isMyServiceRunning(this, ChatHeadService.class))
+                        startService(new Intent(getApplication(), ChatHeadService.class));
+                    else
+                        stopService(new Intent(getApplication(), ChatHeadService.class));
                 }
             }
         }
